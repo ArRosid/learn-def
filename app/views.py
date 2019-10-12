b52ab37f95ae33989b3e13317dcec943dc01bc0b
ArRosid <ahmadrosid30121997@gmail.com>
2019-10-13 06:13:50 +0700

    Simple Rest API

--- app/views.py
+++ app/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(["GET"])
+def home(request):
+    return Response({"message":"Welcome Home!"}, status=status.HTTP_200_OK)
