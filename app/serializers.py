da968bf943264c120bde84b9660ef4be20483f6b
ArRosid <ahmadrosid30121997@gmail.com>
2019-10-16 05:39:51 +0700

    Validation

--- app/serializers.py
+++ app/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
+from rest_framework.serializers import ValidationError
 from . import models
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Article
         fields = "__all__"
+
+    def validate(self, data):
+        if data["title"] == data["description"]:
+            raise ValidationError("Title&description must different")
+        return data
+
+    def validate_title(self, value):
+        if len(value) < 10:
+            raise ValidationError("Title must be more than 10 char")
