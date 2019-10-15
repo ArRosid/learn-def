088c7b6fcf60f8e14c0a08ac81f1458de0c882b5
ArRosid <ahmadrosid30121997@gmail.com>
2019-10-15 10:25:09 +0700

    CRUD Part 2

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('article', views.ArticleListCreateAPIView.as_view())
+    path('article/', views.ArticleListCreateAPIView.as_view()),
+    path('article/<int:pk>/', views.ArticleDetailAPIView.as_view())
 ]
--- app/views.py
+++ app/views.py
@@ -25,3 +25,23 @@ class ArticleListCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ArticleDetailAPIView(APIView):
+    
+    def get(self, request, pk):
+        article = models.Article.objects.get(id=pk)
+        serializer = serializers.ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        article = models.Article.objects.get(id=pk)
+        serializer = serializers.ArticleSerializer(article, request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        article = models.Article.objects.get(id=pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
