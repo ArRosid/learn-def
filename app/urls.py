fc87c028cd7a444398db056452ec553a904a5abf
ArRosid <ahmadrosid30121997@gmail.com>
2019-10-18 07:07:16 +0700

    ViewSet

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from . import views
 
+router = DefaultRouter()
+router.register(r"article", views.ArticleViewSet)
+
 urlpatterns = [
-    path('', views.home),
-    path('article/', views.ArticleListCreateView.as_view()),
-    path('article/<int:pk>/', views.ArticleDetailView.as_view())
+    # path('', views.home),
+    path("", include(router.urls)),
+    # path('article/', views.ArticleViewSet.as_view()),
+    # path('article/<int:pk>/', views.ArticleDetailView.as_view())
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,49 +9,53 @@ from rest_framework.views import APIView
 from . import models
 from . import serializers
 
-@api_view(["GET"])
-def home(request):
-    return Response({"message":"Welcome Home!"},
-                    status=status.HTTP_200_OK)
+# @api_view(["GET"])
+# def home(request):
+    # return Response({"message":"Welcome Home!"},
+    #                 status=status.HTTP_200_OK)
 
 # class ArticleListCreateAPIView(APIView):
     
-    def get(self, request):
-        articles = models.Article.objects.all()
-        serializer = serializers.ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
+    # def get(self, request):
+    #     articles = models.Article.objects.all()
+    #     serializer = serializers.ArticleSerializer(articles, many=True)
+    #     return Response(serializer.data)
 
-    def post(self, request):
-        serializer = serializers.ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request):
+    #     serializer = serializers.ArticleSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # class ArticleDetailAPIView(APIView):
     
-    def get(self, request, pk):
-        article = models.Article.objects.get(id=pk)
-        serializer = serializers.ArticleSerializer(article)
-        return Response(serializer.data)
+    # def get(self, request, pk):
+    #     article = models.Article.objects.get(id=pk)
+    #     serializer = serializers.ArticleSerializer(article)
+    #     return Response(serializer.data)
 
-    def put(self, request, pk):
-        article = models.Article.objects.get(id=pk)
-        serializer = serializers.ArticleSerializer(article, request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def put(self, request, pk):
+    #     article = models.Article.objects.get(id=pk)
+    #     serializer = serializers.ArticleSerializer(article, request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        article = models.Article.objects.get(id=pk)
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    # def delete(self, request, pk):
+    #     article = models.Article.objects.get(id=pk)
+    #     article.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
-class ArticleListCreateView(generics.ListCreateAPIView):
-    queryset = models.Article.objects.all()
+# class ArticleListCreateView(generics.ListCreateAPIView):
+    # queryset = models.Article.objects.all()
+    # serializer_class = serializers.ArticleSerializer
+
+# class ArticleDetailView(generics.RetrieveUpdateDestroyAPIView):
+    # queryset = models.Article.objects.all()
+    # serializer_class = serializers.ArticleSerializer
+
+class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ArticleSerializer
-
-class ArticleDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Article.objects.all()
-    serializer_class = serializers.ArticleSerializer
