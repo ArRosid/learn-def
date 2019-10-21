6f4d53540e0b73fcd12d085ef2b334426950fb7e
ArRosid <ahmadrosid30121997@gmail.com>
2019-10-21 13:41:14 +0700

    Token Based Authentication part 2

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import views
 
@@ -9,7 +8,7 @@ router.register(r"article", views.ArticleViewSet)
 
 urlpatterns = [
     # path('', views.home),
-    path("auth-token-login/", ObtainAuthToken, name="a"),
+    path("login/", views.login),
     path("", include(router.urls)),
     # path('article/', views.ArticleViewSet.as_view()),
     # path('article/<int:pk>/', views.ArticleDetailView.as_view())
--- app/views.py
+++ app/views.py
@@ -61,6 +61,25 @@ from . import serializers
     # queryset = models.Article.objects.all()
     # serializer_class = serializers.ArticleSerializer
 
+@csrf_exempt
+@api_view(['POST'])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if username is None and password is None:
+        return Response({'error':'Please provide both username & password'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    
+    user = authenticate(username=username, password=password)
+
+    if not user:
+        return Response({'error': 'Invalid credentials'},
+                        status=status.HTTP_404_NOT_FOUND)
+    
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({'token':token.key},
+                    status=status.HTTP_200_OK)
+
 class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ArticleSerializer
     queryset = models.Article.objects.all()
